92234bad125650ffb765b6abf80690592294bbac
lowkeyboard <caqlarybak@gmail.com>
2021-04-21 11:00:04 +0300

    login page styled

--- components/auth/Landing.js
+++ components/auth/Landing.js
@@ -1,14 +1,72 @@
-import React from "react";
-import { Text, View, Button } from "react-native";
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  Image,
+  StyleSheet,
+} from 'react-native';
 
-export default function Landing({ navigation }) {
+export default function Landing({navigation}) {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Button
-        title="Register"
-        onPress={() => navigation.navigate("Register")}
-      />
-      <Button title="Login" onPress={() => navigation.navigate("Login")} />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#212530',
+      }}>
+      <Image style={styles.container} source={require('../src/dino.png')} />
+
+      <TouchableOpacity
+        style={styles.logbutton}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.text1}>Log in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.regbutton}
+        onPress={() => navigation.navigate('Register')}>
+        <Text style={styles.text2}>Create Account</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    opacity: 1,
+  },
+
+  logbutton: {
+    backgroundColor: '#2D9CDB',
+    padding: 10,
+    borderRadius: 16,
+    marginRight: 24,
+    marginLeft: 24,
+    width: 327,
+    height: 48,
+  },
+  regbutton: {
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+    borderRadius: 16,
+    marginTop: 20,
+    marginRight: 24,
+    marginLeft: 24,
+    marginBottom: 25,
+    width: 327,
+    height: 48,
+    textAlign: 'center',
+  },
+
+  text1: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+
+  text2: {
+    textAlign: 'center',
+    color: 'black',
+  },
+});
